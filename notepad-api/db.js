0f66116a33eb14f8ea2108948f1ad1b1cabae8ae
Fairuz Muhammad <144330632+NargaFRZ@users.noreply.github.com>
2024-05-18 17:07:20 +0700

    PT09 - Delete host, password, from db.js

--- notepad-api/db.js
+++ notepad-api/db.js
@@ -2,9 +2,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'Fairuz',
-  host: 'ep-odd-wood-a16txrnb-pooler.ap-southeast-1.aws.neon.tech',
+  host: '',
   database: 'Notepad',
-  password: 'z5Qx3DhkiPAZ',
+  password: '',
   port: 5432,
   ssl: {
     require: true,
